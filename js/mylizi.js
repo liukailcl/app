57ac0f24ec349f8b14a4e4ef29d806467314e68d
liukailcl <tuoxuanshe@163.com>
2014-11-10 22:22:15 +0800

    first ci

---
+++ js/mylizi.js
@@ -0,0 +1,15 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+$(document).ready(function() {
+     
+  $('#particles').particleground({ 
+    dotColor: 'rgba(200,20,200,0.3)',
+    lineColor: 'rgba(192,200,200,0.6)',
+   
+  });
+    
+    
+});
